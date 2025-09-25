b6ed72277f435f07ac74c55c6be8cb1d7a9bddff
Vincent Nguyen <vincent.jnguyen@gmail.com>
2025-09-24 23:32:03 -0700

    Implement full Rolodex functionality: add, search, modify, and save contacts; improve input validation and menu prompts; fix file format handling

--- cecs-277-sec8/contact.py
+++ cecs-277-sec8/contact.py
@@ -1,55 +1,105 @@
 # Brian Gutierrez and Vincent Nguyen
+# Wednesday, September 24, 2025
 # Contact class to store contact information from address.txt
 # Each contact has a first name, last name, phone number, address, city, and zip code
 
 class Contact:
-   # Constructor for the contact class
-   # Passes in and assigns the parameters to the class variables
    def __init__(self, fn, ln, ph, addr, city, zip):
-            self.first_name = fn
-            self.last_name = ln
-            self.phone = ph
-            self.address = addr
-            self.city = city
-            self.zip = zip
+      """ 
+      Constructor for the contact class. Passes in and assigns the parameters to the class variables
 
-   # Passes in two contact objects and compares their last names for sorting
-   # Returns boolean value if the first contact's last name is the same as the second contact's last name
+      Args: 
+         fn (str): first name
+         ln (str): last name
+         ph (str): phone number
+         addr (str): address
+         city (str): city
+         zip (str): zip code
+
+      Returns: 
+         None  
+      
+      Raises:
+         None
+      """
+      self.first_name = fn
+      self.last_name = ln
+      self.phone = ph
+      self.address = addr
+      self.city = city
+      self.zip = zip
+
+  
    def __lt__(self, other):
-         if self.last_name != other.last_name:
-            # returns True if self's last name before other's last name
-            return self.last_name < other.last_name 
-         else:
-            # returns True if self's first name before other's first name
-            return self.first_name < other.first_name
+      """
+      Passes in two contact objects and compares their last names for sorting
 
-   # Passes in the object to be displayed in the console
-   # Returns a string with all the contact's information
+      Args:
+         other (Contact): another object to compare to
+         self (Contact): the current object
+         
+      Returns:
+         bool: True if self is less than other, False otherwise
+
+      Raises: 
+         None
+      """
+      if self.last_name != other.last_name:
+         # returns True if self's last name before other's last name
+         return self.last_name < other.last_name 
+      else:
+         # returns True if self's first name before other's first name
+         return self.first_name < other.first_name
+
+
    def __str__(self):
+      """
+      Passes in the object to be displayed in the console
+      
+      Args:
+         self (Contact): the current object
+
+      Returns:
+         str: formatted string with contact details
+
+      Raises:
+         None
+      """
       return (
-         f"First Name: {self.first_name}\n"
-         f"Last Name: {self.last_name}\n"
-         f"Phone #: {self.phone}\n"
-         f"Address: {self.address}\n"
-         f"City: {self.city}\n"
-         f"Zip: {self.zip}\n"
+         f"{self.first_name}\n"
+         f"{self.last_name}\n"
+         f"{self.phone}\n"
+         f"{self.address}\n"
+         f"{self.city}\n"
+         f"{self.zip}\n"
       )
    
-   # Passes in the object to be written to a file
-   # Returns a string that is used in the format 'f_name,l_name,phone,address,city,zip'
+   
    def __repr__(self):
-       return (
-           f"{self.first_name},{self.last_name},{self.phone},"
-           f"{self.address},{self.city},{self.zip}"
+      """
+      Passes in the object to be written to a file 
+
+      Args:
+        self (Contact): the current object
+
+      Returns:
+         str: formatted string for file writing 
+         
+      Raises:
+         None
+      """
+      return (
+         f"{self.first_name},{self.last_name},{self.phone},"
+         f"{self.address},{self.city},{self.zip}"
        )
 
 # Testing class functionality
 def main():
    # Test for class __lt__ method
    print("---------------\n", "Line 18")
-   c1 = contact("Alia", "Smith", "714-2123", "Address1", "Long Beach 1", "11111")
-   c2 = contact("Beck", "Jones", "714-3456", "Address2", "San Clemente 2", "22222")
-   c3 = contact("Carol","Smith", "714-3789", "Address3", "Los Angeles 3", "33333")
+   c1 = Contact("Alia", "Smith", "714-2123", "Address1", "Long Beach 1", "11111")
+   c2 = Contact("Beck", "Jones", "714-3456", "Address2", "San Clemente 2", "22222")
+   c3 = Contact("Carol","Smith", "714-3789", "Address3", "Los Angeles 3", "33333")
    print(c1.first_name)  # testing "Alia"
    print(c1 < c2)        # False
    print(c1 < c3, "\n")  # True
--- cecs-277-sec8/lab5.py
+++ cecs-277-sec8/lab5.py
@@ -1,35 +1,51 @@
 # Brian Gutierrez and Vincent Nguyen
-# Contact class to store contact information from address.txt
-# Each contact has a first name, last name, phone number, address, city, and zip code
+# Wednesday, September 24, 2025
+# The Main.py that runs Rolodex which allows the user to view, search, and modify a contact list 
+# made up of contact objects. The contact is imported from a contact class which has the 
+# name, phone number, address, city, and zip code from 'addresses.txt'
 
 import check_input
 from contact import Contact
 
-# Opens and reads one line creating a contact object and then stores it in a list.
-# Sorts and then returns the list.
 def read_file():
+   """
+   Opens and reads one line creating an object and then stores it in a list.
+   
+   Args:
+      None
+   
+   Returns:
+      list: list of objects from the Contact class
+
+   Raises:
+      FileNotFoundError: if the file does not exist
+   """
    contacts = []
    try:
       with open("addresses.txt", "r") as file:
+         # Iterates through each line in the file
          for line in file:
-            line = file.readline().strip()
-            fields = line.split(",") # Splits the line into a list of fields
-            if len(fields) != 6:
-               print("Invalid line format:", line)
-               continue  # skips to the next line
+            line = line.strip()      # Gets rid of white space in the front and back of the line
+            fields = line.split(",") # Splits the line into a list of fields based on commas
+
+            if len(fields) != 6:     # checks if the line has exactly 6 fields
+               print("Invalid format:", line)
+               continue              # skips to the next line
+
+            # Assigns each field to a variable after stripping whitespace
             for i in range(len(fields)):
-               fields[i] = fields[i].strip() # Removes xtra whitespace
-               firstName = fields[0]
-               lastName = fields[1]
-               phone = fields[2]
-               address = fields[3]
-               city = fields[4]
-               zipCode = fields[5]
-            # Calls Contact constructor from our import and appends it to the list
-            contacts.append(Contact(firstName, lastName, phone,
-                                     address, city, zipCode))
-            # uses __lt__ method from contact.py to sort. This is because the list 
-            # contains Contact objects
+               fields[i] = fields[i].strip()
+               first_name_str = fields[0]
+               last_name_str = fields[1]
+               phone_str = fields[2]
+               address_str = fields[3]
+               city_str = fields[4]
+               zip_code_str = fields[5]
+            # Calls Contact constructor from the import and appends it to the list
+            contacts.append(Contact(first_name_str, last_name_str, phone_str,
+                                     address_str, city_str, zip_code_str))
+            # uses __lt__ method from contact.py to sort. This is because the list
+            # contains objects from the Contact class
             contacts.sort()
       return contacts
    
@@ -37,11 +53,225 @@ def read_file():
       print("File not found.")
       return contacts
 
+
+def write_file(contacts):
+   """
+   Passes in the list of contacts and opens the file for writing, loop through the list
+   and writes each contact to the file using the __repr__ method from contact.py
+
+   Args:
+      contacts (list): list of objects to write to the file
+
+   Returns:
+      None
+   
+   Raises:
+      FileNotFoundError: if the file does not exist
+   """
+   # Checks if we can open the file
+   try:
+      with open("addresses.txt", "w") as file:
+         # Iterates through the list 
+         for contact in contacts:
+            file.write(repr(contact) + "\n") 
+   except FileNotFoundError:
+            print("File not found.")
+            return
+   
+
+def get_menu_choice():
+   """
+   Displays the main menu to the user and then takes user input
+ 
+   Args:
+      None
+
+   Returns:
+      int: user input from 1-5
+   
+   Raises:
+      None 
+   """
+   print("Rolodex Menu:")
+   print("1. Display Contacts")
+   print("2. Add Contact")
+   print("3. Search Contacts")
+   print("4. Modify Contact")
+   print("5. Save and Quit Program")
+   user_input = check_input.get_int_range(">  ", 1, 5)
+   return user_input
+
+
+def modify_contact(contact):
+   """
+   Displays the modify menu and takes user input to change the contact information
+   
+   Args:
+      contact (Contact): object to be modified
+
+   Returns:
+      Contact: the modified contact
+   
+   Raises:
+      None
+   """
+   print("Modify Menu")
+   print("1. First Name")
+   print("2. Last Name")
+   print("3. Phone")
+   print("4. Address")
+   print("5. City")
+   print("6. Zip Code")
+   print("7. Save and Exit Modify Menu\n") 
+
+   # Loops until user chooses to exit the modify menu
+   while True:
+      print("Select field to modify or exit:")
+      user_input = check_input.get_int_range(">  ", 1, 7)
+      match user_input:
+         case 1:
+            new_first_name = input("Enter new first name: ")
+            contact.first_name = new_first_name
+         case 2:
+            new_last_name = input("Enter new last name: ")
+            contact.last_name = new_last_name
+         case 3:
+            new_phone_number = input("Enter new phone number: ")
+            contact.phone = new_phone_number
+         case 4:
+            new_address = input("Enter new address: ")
+            contact.address = new_address
+         case 5:
+            new_city = input("Enter new city: ")
+            contact.city = new_city
+         case 6:
+            new_zip_code = input("Enter new zip code: ")
+            contact.zip_code = new_zip_code
+         case 7:
+            break
+   return contact
+
+
 def main():
-   # Test for read_file function
-   contacts = read_file()
-   for contact in contacts:
-      print(contact)
+   """
+   Main to run the contact management program
+   Displays the menu and takes user input to call the right function
+   to manage the contacts
+   """
+   contacts = read_file() # Load contacts from file at start
+   while True:
+      user_choice = get_menu_choice()
+      # Matches user input to the appropriate Rolodex menu option 1-5
+      match user_choice:
+         # 1. Display Contacts, reads the file and displays an enumerated list of contacts
+         # of each of the contacts dusing the __str__ method from contact.py
+         case 1:
+            contacts = read_file()
+            num_of_contacts = check_input.get_int_range("Number of contacts: ", 0, len(contacts))
+            contact = contacts[num_of_contacts - 1]  # num_of_contacts is 1 based index
+            # Displays the contact information using __str__ method from contact.py
+            if num_of_contacts > 0 and len(contacts) > 0: 
+               display_num = 1
+               # Iterates through the list contacts and displays only the number of contacts requested
+               for contact in contacts[:num_of_contacts]:  
+                  print(f"{display_num}: {contact.first_name} {contact.last_name} ")
+                  print(f"   {contact.phone}")
+                  print(f"   {contact.address}")
+                  print(f"   {contact.city}")
+                  print(f"   {contact.zip}")
+                  print()
+                  display_num += 1
+            pass
+
+         # 2. Add Contact - prompts user to enter each piece of data for a contact,
+         # constructing the contact object and adding it to the list of contacts and then sorting 
+         case 2:
+            # Get user input for the new contact information
+            print("Enter new contact:")
+            first_name = input("First name: ")
+            last_name = input("Last name: ")
+            phone = input("Phone #: ")
+            address = input("Address: ")
+            city = input("City: ")
+            zip_code = input("Zip code: ")
+            print()
+            # Checks for any repeated people    
+            for contact in contacts:
+               if (contact.first_name.lower().strip() == first_name.lower().strip() and 
+                   contact.last_name.lower().strip() == last_name.lower().strip()):
+                   print("Contact already exists.")
+                   break
+            else:
+               contacts.append(Contact(first_name, last_name, phone, address, city, zip_code))
+               contacts.sort()
+               write_file(contacts)
+               print("Contact added.\n")
+            pass
+         
+         # 3. Search Contacts - prompts user to enter the type of search, by last name 
+         # or zip code, then prompts for search and then loops through the list and prints
+         # Displays all contacts that match the search requirements
+         case 3:
+            print("Search:")
+            print("1. Search by last name")
+            print("2. Search by zip code")
+
+            # Loops until user chooses to exit the search menu
+            # Checks if user wants to search by last name or zip code
+            search_choice = check_input.get_int_range("> ", 1, 2)
+            # Checks if user wants to exit search menu
+
+            if search_choice == 1:
+               search_last_name = input("1. Search by last name: ")
+               found = False # Set a flag to see if we found a match
+               # Finds and prints the contacts that match the last name
+               for contact in contacts:
+                  if contact.last_name.lower().strip() == search_last_name.lower().strip():
+                     print(contact)
+                     found = True
+               if not found:
+                  print("No contact found with that last name.\n")
+           
+            # Checks if user wants to search by zip code
+            elif search_choice == 2:
+               search_zip_code = input("2. Search by zip code: ")
+               found = False 
+               # Iterates through the list and prints the contacts with the matching zip code 
+               for contact in contacts:
+                  if contact.zip.strip() == search_zip_code.strip():
+                     print(contact)
+                     found = True
+               if not found:
+                  print("No contact found with that zip code.\n")
+            # If user enters invalid choice
+            else:
+               print("Invalid choice.")
+         
+         # 4. Modify Contact -  prompts user to enter the first and last name of the contact to modify
+         # if found, calls the modify_contact function to display the modify menu and take user input
+         case 4:
+            first_name = input("Enter first name: ")
+            last_name = input("Enter last name: ")
+            found = False
+            # Iterates through the list to find the matching first and last name
+            for contact in contacts:
+               if (contact.first_name.lower().strip() == first_name.lower().strip() and
+                  contact.last_name.lower().strip() == last_name.lower().strip()):
+                  found = True
+                  print(contact)
+                  modify_contact(contact)
+                  write_file(contacts)  # saves the changes
+                  print("Contact has been modified.\n")
+                  break
+            if not found:
+               print("ERROR: Person could not be found")
+
+         # 5. Save and Quit Program - writes the list of contacts to the file and exits the program
+         case 5:
+            print("Saving...")
+            write_file(contacts)
+            print("Ending Program")
+            break
 
 
 if __name__ == "__main__":
