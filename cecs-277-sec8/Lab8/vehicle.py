9f0069f4d1743ce322ed1c89bc7c29dbf419fcf5
Vincent Nguyen <vincent.jnguyen@gmail.com>
2025-10-15 01:06:56 -0700

    Add initial empty files for car, motorcycle, truck, and vehicle classes

---
+++ cecs-277-sec8/Lab8/vehicle.py
@@ -0,0 +1,14 @@
+"""
+Brian Gutierrez and Vincent Nguyen
+CECS 277 - Sec 8
+Lab 8
+11/06/2023
+vehicle.py
+"""
+
+class Vehicle:
+   """
+   An Abstract Class that creates a name, label, speed, position and
+   current location on track and vehicle's power level.
+   """  
+   
