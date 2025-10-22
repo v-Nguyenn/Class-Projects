7b71eb598239453f743bf8d5186b63f925ad3f1b
Vincent Nguyen <vincent.jnguyen@gmail.com>
2025-10-22 14:04:54 -0700

    Adds door.py with abstract methods.
    Adds Lab 9 pdf to gitignore

--- cecs-277-sec8/Lab9/door.py
+++ cecs-277-sec8/Lab9/door.py
@@ -27,4 +27,60 @@ class Door(ABC):
    def menu_options(self):
       """
       Returns a string of the menu options that user
+      can choose when attempting to unlock the door.
+
+      Returns:
+         str: Menu options for the door.
       """
+      pass
+
+   @abstractmethod
+   def get_menu_max(self): 
+      """
+      Returns the number of options in the above menu.
+
+      Returns:
+         int: The number of options
+      """
+      pass
+
+   @abstractmethod
+   def attempt(self, option):
+      """
+      Determines whether what user selected unlocks the door.
+
+      Returns:
+         str: Message describing what the user attempted.
+      """
+      pass
+
+   @abstractmethod
+   def is_unlocked(self):
+      """
+      Checks to see if the the door was unlocked.
+
+      REturns:
+         bool: True if unlocked, false if locked.
+      """
+      pass
+
+   @abstractmethod
+   def clue(self):
+      """
+      A hint to help the user if user was unsuccessful 
+
+      Returns:
+         str: Message to assist user
+      """
+      pass
+      
+
+   @abstractmethod
+   def success(self):
+      """
+      Message that is returned if the user is successful.
+
+      Returns: 
+         str: Message congratulating the user
+      """
+      pass
