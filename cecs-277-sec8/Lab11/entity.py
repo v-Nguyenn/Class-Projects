ff6eb11258c1e9616bbd9738824705922f592d7e
Vincent Nguyen <vincent.jnguyen@gmail.com>
2025-11-03 13:06:53 -0800

    Add Entity class and Hero subclass; include lab documentation and factory PDF

---
+++ cecs-277-sec8/Lab11/entity.py
@@ -0,0 +1,37 @@
+"""
+Brian Gutierrez and Vincent Nguyen
+Lab 11
+CECS 277 Section 8
+Fall 2025
+November 3, 2025
+"""
+from abc import ABC, abstractmethod
+
+class Entity(ABC):
+   """ Abstracts the monsters and hero which extend from. """
+   def __init__(self, name, hp):
+      """ Sets the name and hp attributes """
+      self._name = name
+      self._hp = hp
+
+   @property
+   def name(self):
+      """ Decorator to get the values of _name """
+      return self._name
+
+   @property
+   def hp(self):
+      """ Decorator to get the values of _hp"""
+      return self._hp
+
+   def take_damage(self, dmg):
+      """ Method deals the damage the entity, Subtracting the damage from the entity's HP value """
+      self._hp = max(0, self._hp - dmg)
+
+   def __str__(self):
+      """ Returns a string with the entity's name and hp """
+      return f"{self._name}, HP: {self._hp}"
+
+   def melee_attack(self, enemy):
+      " The attack the entity does to another entity "
+      pass
---
+++ cecs-277-sec8/Lab11/hero.py
@@ -0,0 +1,13 @@
+"""
+Brian Gutierrez and Vincent Nguyen
+Lab 11
+CECS 277 Section 8
+Fall 2025
+November 3, 2025
+"""
+
+from entity import Entity
+
+class Hero(Entity):
+   """ The User's character extending from entity """
+   
