a04dcb97b1261c7fd255ec00fc2f39306d697b2f
Vincent Nguyen <vincent.jnguyen@gmail.com>
2025-11-11 15:38:11 -0800

    Add abstract methods for description, area, weight, and count in Plate class

--- cecs-277-sec8/Lab12/plate.py
+++ cecs-277-sec8/Lab12/plate.py
@@ -12,4 +12,22 @@ from abc import ABC, abstractmethod
 
 class Plate():
    """ Plate which has a description area, weight, and count """
-   
+   @abstractmethod
+   def description(self):
+      """ Returns a string description of the plate and what is on it """
+      pass
+
+   @abstractmethod
+   def area(self):
+      """ Returns the remaining square inches of the plate can hold """
+      pass
+
+   @abstractmethod
+   def weight(self):
+      """ Returns the remaining number of ounces the plate can hold. """
+      pass
+
+   @abstractmethod
+   def count(self):
+      """ Returns the number of food items the plate is currently holding. """
+      pass      
