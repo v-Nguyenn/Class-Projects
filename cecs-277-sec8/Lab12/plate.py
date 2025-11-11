c4f4023ed109229f9f49e9e47c4b0eaf19d3195a
Vincent Nguyen <vincent.jnguyen@gmail.com>
2025-11-10 23:57:29 -0800

    Add lab PDF for CECS 277 Lab 12 on Decorator Pattern

---
+++ cecs-277-sec8/Lab12/plate.py
@@ -0,0 +1,15 @@
+"""
+Brian Gutierrez and Vincent Nguyen
+Lab 12
+CECS 277 Section 8
+Fall 2025
+11/10/2025
+
+This is the interface for plate which extends to small_plate and Big plate 
+"""
+
+from abc import ABC, abstractmethod
+
+class Plate():
+   """ Plate which has a description area, weight, and count """
+   
