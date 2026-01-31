39753077ee85a609f479ca050e176456e6ddc0d4
Vincent Nguyen <vincent.jnguyen@gmail.com>
2026-01-30 17:57:37 -0800

    refactor: include endpoint in range iteration for congruent_to function

--- cecs-229-sec1/Assignment1/pa1.py
+++ cecs-229-sec1/Assignment1/pa1.py
@@ -4,7 +4,8 @@
 
 def congruent_to(a, m, low, high):
     x_vals = []
-    for val in range(low, high): 
+    # Use high + 1 to include the endpoint 
+    for val in range(low, high + 1): 
         # Checks if val and a have the same remainder when divided by m
         if val % m == a % m:
             x_vals.append(val) 
