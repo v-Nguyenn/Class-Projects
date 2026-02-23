4b7734bfba71e1f7861a5a237b0ce59dd430cd00
Vincent Nguyen <vincent.jnguyen@gmail.com>
2026-02-23 07:03:56 -0800

    fix: update learning objectives and enhance program documentation for One Card War

--- cecs-325-sec1/Program2/war.cpp
+++ cecs-325-sec1/Program2/war.cpp
@@ -1,3 +1,32 @@
 // Author: Vincent Nguyen
 // CECS 325-0
 // Program 2 - One Card War
+// Due 02/23/2026
+// 
+// I certify that this program is my own original work. I did not copy 
+// any part of this program from any other source including AI use. I 
+// did not share this program with anyone. I further certify that I typed
+// each and every line of code in this program
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Card
+{
+
+}
+
+class Deck
+{
+   
+}
+
+struct Player
+{
+   string name;
+   int win;
+   int lose;
+   int tie;
+}
