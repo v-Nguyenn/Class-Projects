387df22dcfdc40cc8090a9835d03d4634574e888
Vincent Nguyen <vincent.jnguyen@gmail.com>
2026-03-04 07:52:59 -0800

    Add mysort.cpp with comments for now

---
+++ cecs-325-sec1/Program3/mysort.cpp
@@ -0,0 +1,10 @@
+// Author: Vincent Nguyen
+// CECS 325-0
+// Program 3 - Sorting Contest
+//
+// I certify that this program is my own original work. I did not copy
+// any part of this program from any other source including AI use. I
+// did not share this program with anyone. I further certify that I typed
+// each and every line of code in this program.
+
+#include <iostream>
