09a6a0325461e5744a4906f58a881eb87958a8d0
Vincent Nguyen <vincent.jnguyen@gmail.com>
2026-03-09 22:41:05 -0700

    Implement hift_decrypt and RSA encryption/decryption logic. Added helper functions for gcd and bezout coefficients.

--- cecs-229-sec1/Assignment5/pa5.py
+++ cecs-229-sec1/Assignment5/pa5.py
@@ -44,7 +44,46 @@ def bezout_coeffs(a, b):
         :param b: positive integer > 0
         :returns: tuple of the bezout coefficients (s, t).
     """
+    # Iteratively or recursively calculate the values s and t using the Extended Euclidian Algorithm.
+    old_s, s = 1, 0
+    old_t, t = 0, 1
+    old_r, r = a, b
 
+    while r != 0:
+        quotient = old_r // r
+        old_r, r = r, old_r - quotient * r
+        old_s, s = s, old_s - quotient * s
+        old_t, t = t, old_t - quotient * t
+
+        # Return the final values for s and t as a tuple (s, t)
+    return (old_s, old_t)
+
+
+def gcd(a, b):
+    """ Uses the Euclidian Algorithm to compute the greatest common divisor of two integers, where a >= b.
+        :param a: a non-zero integer (positive or negative).
+        :param b: a non-zero integer (positive or negative) that is less than or equal to a.
+        :returns: the gcd of a and b.
+        :raises: ValueError if a or b is 0 or b > a.
+    """
+    # Update a and b for the Euclidian Algorithm
+    a = abs(a)
+    b = abs(b)
+
+    # throw an exception if the parameters a and/or b have invalid values
+    if a == 0 or b == 0:
+        raise ValueError("Invalid parameter value.")
+    if b > a:
+        a, b = b, a
+    
+    # Use the Euclidian Algorithm to iteratively find the gcd of a and b.
+    while b != 0:
+        temp = a % b
+        a = b
+        b = temp
+
+    # return the gcd of a and b.
+    return a 
 
 def mod_inv(a, m):
     """ Computes the inverse of value a under modulus m.
@@ -53,6 +92,22 @@ def mod_inv(a, m):
         :returns: the integer inverse of a under modulus m in range [0, m-1].
         :raises: ValueError if m < 1 or if a and m are not relatively prime.
     """
+    if m < 1: # TODO: check modulus m for validity.
+        raise ValueError(f"mod_inv(a, m) does not support 0 or negative modulus m = {m}.")
+
+    if gcd(a, m) != 1 : # TODO: check that the gcd is relatively prime 
+        raise ValueError(f"mod_inv(a, m) does not support integers that are not relatively prime.")
+
+    # reduce value a to be in range [0, m-1]
+
+    # use the Extended Euclidian Algorithm to find the Bezout coefficient of value a.
+    # s is coefficient for a and b is coefficient for t: a * s + b * t = gcd(a.b)
+    a = a % m
+    s, t = bezout_coeffs(a, m)
+    inverse = s % m
+
+    # reduce the calculated inverse to be in range [0, m-1]
+    return inverse
 
 
 # ----------------- PROBLEM 1 ----------------- #
@@ -104,6 +159,15 @@ def shift_decrypt(text, k):
     # TODO: iterate through each character in the ciphertext, if it is a space, add
     # it to the decryption string, otherwise apply the reversed shift function using 
     # the key and add the result to the decryption string.
+    for ch in text:
+        if ch == " ":
+            s += ch
+        else:
+            num = char_to_num(ch)
+            # We do - k to reverse the encryption
+            shifted = (int(num) - k) % 26
+            decryptedCh = num_to_char(shifted)
+            s += decryptedCh
     
     # return the decryption string
     return s
@@ -124,21 +188,27 @@ def rsa_encrypt(text, n, e):
     text = text.upper()
     
     # TODO: find blocksize
-    block_size = 
+    block_size = blocksize(n)
 
     # TODO: pad with X if text will be insufficient to fill all blocks
-
+    while (2 * len(text)) % block_size != 0:
+        text += "X"
 
     # TODO: convert text to numbers
     numbers = ""
-
+    for ch in text:
+        numbers += char_to_num(ch)
 
     # TODO: separate the string of numbers into a list of blocks
     blocks = []
+    for i in range(0, len(numbers), block_size):
+        blocks.append(numbers[i:i + block_size])
 
     # TODO: encrypt each block and build the ciphertext
     cipher = ""
-
+    for block in blocks:
+        c = pow(int(block), e, n)
+        cipher += str(c).zfill(block_size)
         
     # return the ciphertext
     return cipher
@@ -161,20 +231,27 @@ def rsa_decrypt(text, p, q, e):
     :returns: the decrypted text as a string.
     """
     # TODO: calculate n
-    n = 
+    n = p * q
 
     # TODO: find blocksize
-    block_size = 
+    block_size = blocksize(n)
 
     # TODO: separate the encrypted string of numbers into a list of blocks
     blocks = []
+    for i in range(0, len(text), block_size):
+        blocks.append(text[i:i + block_size])
 
     # TODO: decrypt each block and build the message as a string of numbers
     numbers = ""
-
+    d = mod_inv(e, (p - 1) * (q - 1))
+    for block in blocks:
+        m = pow(int(block), d, n)
+        numbers += str(m).zfill(block_size)
 
     # TODO: convert each of the 2 digit numbers into letters
     decrypted_text = ""
+    for i in range(0, len(numbers), 2):
+        decrypted_text += num_to_char(numbers[i:i + 2])
 
         
     # return the dectrypted text
