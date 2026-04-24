d8bce84dd916d78fdefe90dce26db834c8e1d9de
Vincent Nguyen <vincent.jnguyen@gmail.com>
2026-04-23 21:21:33 -0700

    Add initial test framework for Problems 1 and 2

---
+++ cecs-229-sec1/Assignment10/your_tester.py
@@ -0,0 +1,10 @@
+import pa8
+import pa10
+
+def test():
+    print("Testing Problem 1...")
+    # Write your test cases for Problem 1 here
+
+    print("Testing Problem 2...")
+    # Write your test cases for Problem 2 here
+    
