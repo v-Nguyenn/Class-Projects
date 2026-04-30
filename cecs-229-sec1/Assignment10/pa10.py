60db588d9ed1602feaf6b7e973cf024efac87356
Vincent Nguyen <vincent.jnguyen@gmail.com>
2026-04-30 01:02:33 -0700

    Refactor submatrix and determinant functions to improve error handling and implement missing logic

--- cecs-229-sec1/Assignment10/pa10.py
+++ cecs-229-sec1/Assignment10/pa10.py
@@ -1,3 +1,6 @@
+# Vincent Nguyen 
+# Due: April 30, 2026
+
 import pa8
 
 def submatrix(A, i, j):
@@ -5,11 +8,17 @@ def submatrix(A, i, j):
     Raises an IndexError if i or j are not within the dimentions of A.
     """
     m, n = A.dim()
-    if #TODO: check to see if i and j are within the dimensions of A.
+    #TODO: check to see if i and j are within the dimensions of A.
+    if i < 1 or i > m or j < 1 or j > n:   
         raise IndexError(
             f"Submatrix omitting row {i} and column {j} can not be created for Matrix with dimension {m}x{n} ")
     rows = []
     #TODO: build and return the submatrix of A with row i and column j removed.
+    for r in range(1, m + 1):
+        if r == i:
+            continue
+        row = [A.get_element(r, c) for c in range(1, n + 1) if c != j]
+        rows.append(row)
 
     return pa8.Matrix(rows)
 
@@ -19,16 +28,19 @@ def determinant(A):
     Raises a ValueError if the matrix is not square.
     """
     m, n = A.dim()
-    if #TODO: check to see if matrix A is a square matrix
+    if m != n #TODO: check to see if matrix A is a square matrix
         raise ValueError(f"Determinant is not defined for Matrix with dimension {m}x{n}.  Matrix must be square.")
 
     if n == 1:
-        return #TODO: return the determinant if n = 1
+        return A.get_element(1, 1) #TODO: return the determinant if n = 1
     elif n == 2:
-        return #TODO: calculate and return the determinant if n = 2
+        #TODO: calculate and return the determinant if n = 2
+        return A.get_element(1, 1) * A.get_element(2, 2) - A.get_element(1, 2) * A.get_element(2, 1)
+        
     else: 
         #TODO: calculate and return the determinant if n >= 3
-        d = 0
-
+        for j in range(1, n + 1):
+            cofactor = ((-1) ** (1 + j)) * A.get_element(1, j) * determinant(submatrix(A, 1, j))
+            d += cofactor
         return d
 
