1bf8a698ec33033cb10e972d7c59600b55130925
Vincent Nguyen <vincent.jnguyen@gmail.com>
2026-05-01 22:38:12 -0700

    Add additional test cases for determinant function in your_tester.py

--- cecs-229-sec1/Assignment10/your_tester.py
+++ cecs-229-sec1/Assignment10/your_tester.py
@@ -9,6 +9,17 @@ def test():
     expected = pa8.Matrix([[1, 3], [7, 9]])
     print("submatrix removes row 2, col 2:", "PASS" if result == expected else "FAIL")
 
+
+
     print("Testing Problem 2...")
     # Write your test cases for Problem 2 here
-    
+    A = pa8.Matrix([[1, 2], [3, 4]])
+    result = pa10.determinant(A)
+    expected = -2
+    print("determinant 2x2:", "PASS" if result == expected else "FAIL")
+
+    print("Testing Problem 3...")
+    A = pa8.Matrix([[1, -3, 0], [-2, 4, 1], [5, -2, 2]])
+    result = pa10.determinant(A)
+    expected = -17
+    print("determinant 3x3:", "PASS" if result == expected else "FAIL")
