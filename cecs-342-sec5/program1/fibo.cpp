30414d73c246f83e89342f144a40e1052357a720
Vincent Nguyen <vincent.jnguyen@gmail.com>
2026-08-26 23:58:04 -0700

    Add initial implementation of Fibo Race program in C++ and Python

---
+++ cecs-342-sec5/program1/fibo.cpp
@@ -0,0 +1,10 @@
+//    Name: Vincent Nguyen
+//    Class: CECS 342 Section 5
+//    Project Name: Prog 1 - Fibo Race
+//    Due Date: 09/03/2026
+//
+//    I certify that this program is my own original work. I did not 
+//    copy any part of this program from any other source including
+//    inappropriate AI use. I did not share this program with anyone. I 
+//    further certify that I typed each and every line of code in this program.
+
