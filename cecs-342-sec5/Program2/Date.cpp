82fb9464f2dc08384bf85b24c39565ecc947e216
Vincent Nguyen <vincent.jnguyen@gmail.com>
2026-09-08 22:50:30 -0700

    Added the date class

---
+++ cecs-342-sec5/Program2/Date.cpp
@@ -0,0 +1,23 @@
+
+// Figuring out Leap Year ?
+// if year div by 4, leapyear
+// if year div by 100 not leap year
+// if year div by 400 is leap 
+
+// How to know what day it is ?
+// Julian day mod 7 to figure out day?  
+
+class Date
+{
+private:
+int *dptr;
+
+public:
+// Constructor 
+Date()
+{
+   dptr = new int[3],
+   dptr
+}
+
+};
