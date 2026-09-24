8752a824053c11ae7c3f897879c3de6db269e669
Vincent Nguyen <vincent.jnguyen@gmail.com>
2026-09-23 23:20:39 -0700

    add Date constructor and methods for month/day/year and Julian date conversion, including getMonthName and getDayName functions, gregorian to Julian conversion, and a main function for testing.

--- cecs-342-sec5/Program2/Date.cpp
+++ cecs-342-sec5/Program2/Date.cpp
@@ -16,6 +16,7 @@
 // How to know what day it is ?
 // Julian day mod 7 to figure out day?  
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Date
@@ -37,25 +38,52 @@ class Date
 
    // Overloaded constructor (Set the date for valid arguments are provided).
    // Falls back to Default construtor if not set using : Date() 
-   Date(int Month, int Day, int Year) : Date()
+   Date(int month, int day, int year) : Date()
    {
       datePtr = new int[3];
-      datePtr[0] = Month;
-      datePtr[1] = Day;
-      datePtr[2] = Year;
+      datePtr[0] = month;
+      datePtr[1] = day;
+      datePtr[2] = year;
       count++;
    }
 
-   // Overloaded constructor to Julian date based on Unix Epoch time
-   Date(int JulianDate)
+   // Overloaded constructor to create a date using the Julian
+   Date(int julianDate)
    {
-      int Month, Day, Year;
+      // Based on Fliegel and van Flandern Julian date to Gregorian date.
+      // Kept variables to match the temp variables used for the math.
+      // Changed I, J, K to year, month, day respectively.
+      int year, month, day, L, N; 
+      L = julianDate + 68569;
+      N = 4 * L / 146097;
+      L = L - (146097 * N + 3) / 4;
+      year = 4000 * (L + 1) / 1461001;
+      L = L - 1461 * year / 4 + 31;
+      month = 80 * L / 2447;
+      day = L - 2447 * month / 80;
+      L = month / 11;
+      month = month + 2 - 12 * L;
+      year = 100 * (N - 49) + year + L;
+      datePtr[0] = month;
+      datePtr[1] = day;
+      datePtr[2] = year;
+      count++;
+   }
+
+   // Function to convert Gregorian to Julian date 
+   int gregToJulian(int month, int day, int year)
+   {
+      int month, day, year;
+      month = getMonth();
+      day = getDay();
+      year = getYear();
       // Based on Fliegel and van Flandern computer algorithm for converting
-      // between Julian dates in Fortran. 
-      JulianDate = Day - 32075 + 1461 * 
-         (Year + 4800 + (Month - 14) / 12) / 4 + 367 *
-         (Month - 2 - (Month - 14) / 12 * 12) / 12 - 3 * 
-         ((Year + 4900 + (Month - 14) / 12) / 100 / 4);
+      // between Julian dates in Fortran. Changed variables to be readable.
+      int julianDate = day - 32075 + 1461 * 
+         (year + 4800 + (month - 14) / 12) / 4 + 367 *
+         (month - 2 - (month - 14) / 12 * 12) / 12 - 3 * 
+         ((year + 4900 + ((month - 14) / 12) / 100) / 4);
+      return julianDate;
    }
 
    // Destructor (de-allocate any memory assigned in constructor)
@@ -72,16 +100,58 @@ class Date
       return datePtr[0];
    }
 
-   //
+   // Returns the day in integer form
    int getDay()
    {
       return datePtr[1];
    }
    
+   // Returns the year in integer form
    int getYear()
    {
       return datePtr[2];
    }
 
+   // Returns the name of the month - 3 letters only
+   string getMonthName()
+   {
+      int month = datePtr[0];
+      if (month == 1) return "Jan";
+      else if (month == 2) return "Feb";
+      else if (month == 3) return "Mar";
+      else if (month == 4) return "Apr";
+      else if (month == 5) return "May"; 
+      else if (month == 6) return "Jun";
+      else if (month == 7) return "Jul";
+      else if (month == 8) return "Aug";
+      else if (month == 9) return "Sep";
+      else if (month == 10) return "Oct";
+      else if (month == 11) return "Nov"; 
+      else if (month == 12) return "Dec";
+      return "Please enter a valid integer date (1 - 12).";
+   }
+
+   // Returns the week day name, Monday, Tuesday, etc 
+   string getDayName()
+   {
+      int m = getMonth();
+      int d = getDay();
+      int y = getYear();
+      int julianNumber = gregToJulian(m, d, y); // Grab Julian date
+      int day = julianNumber % 7 + 1; // Mod julian number to get day of week 1 - 7  
+      if (day == 1) return "Monday";
+      else if (day == 2) return "Tuesday";
+      else if (day == 3) return "Wednesday";
+      else if (day == 4) return "Thursday";
+      else if (day == 5) return "Friday";
+      else if (day == 6) return "Saturday";
+      else if (day == 7) return "Sunday";
+      return "Not a valid day. ";
+   }
 
 };
+
+int main(){
+   cout << "My own test" << endl;
+   Date d; // Test default constructor
+}
